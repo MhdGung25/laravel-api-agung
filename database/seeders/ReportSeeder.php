49cde9ec46bb0eb0695ff16f55023e2b8d186507
Muhammad Agung Pamungkas <muhammadgung2003@gmail.com>
2026-04-21 16:50:54 +0700

    Update fitur API dan optimasi query

--- database/seeders/ReportSeeder.php
+++ database/seeders/ReportSeeder.php
@@ -3,7 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Report; // Tambahkan import ini agar tidak error "not found"
+use App\Models\Report; 
 use App\Models\User;
 
 class ReportSeeder extends Seeder
